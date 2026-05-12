4cfb80a236cf4d9a131806fee84d380fc91bb8ca
matheosamurai80-source <matheosamurai80@gmail.com>
2026-05-12 20:44:34 +0200

    deploy: src/app/onboarding/page.tsx

--- src/app/onboarding/page.tsx
+++ src/app/onboarding/page.tsx
@@ -1,5 +1,2 @@
 import { OnboardingShell } from '@/components/onboarding/OnboardingShell'
-
-export default function OnboardingPage() {
-  return <OnboardingShell />
-}
+export default function OnboardingPage() { return <OnboardingShell /> }
